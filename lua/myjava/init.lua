1d38cd1a00d35a4ed58fc035f251a5181c6814d4
Sven12345re <sven.redlich96@gmail.com>
2025-09-24 01:07:53 +0200

    test

--- lua/myjava/init.lua
+++ lua/myjava/init.lua
@@ -7,8 +7,10 @@ function M.greeting()
 end
 
 function M.compileAndnRun()
-  local mainFileName = os.execute('grep -H "public static void main" *.java | cut -d: -f1')
-	os.execute('javac -d class " + mainFileName)
+  local handle = io.popen('grep -H "public static void main" *.java | cut -d: -f1')
+  local result = handle:read("*a")  -- liest die gesamte Ausgabe
+  handle:close()
+  os.execute('javac -d class " + result)
 	os.execute("java HelloWorld")
 end
 
