6af4d15430ad2937deb4d54b7b496d2d68287129
Sven12345re <sven.redlich96@gmail.com>
2025-09-24 01:13:23 +0200

    concat string fix

--- lua/myjava/init.lua
+++ lua/myjava/init.lua
@@ -10,7 +10,7 @@ function M.compileAndnRun()
 	local handle = io.popen('grep -H "public static void main" *.java | cut -d: -f1')
 	local result = handle:read("*a") -- liest die gesamte Ausgabe
 	handle:close()
-	os.execute("javac -d class " + result)
+	os.execute("javac -d class " .. result)
 	os.execute("java HelloWorld")
 end
 
