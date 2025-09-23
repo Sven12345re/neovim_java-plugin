3463f98e3a85de957fa9812b3a0a0553762590ce
Sven12345re <sven.redlich96@gmail.com>
2025-09-24 01:11:48 +0200

    fixed unfinished string

--- lua/myjava/init.lua
+++ lua/myjava/init.lua
@@ -7,10 +7,10 @@ function M.greeting()
 end
 
 function M.compileAndnRun()
-  local handle = io.popen('grep -H "public static void main" *.java | cut -d: -f1')
-  local result = handle:read("*a")  -- liest die gesamte Ausgabe
-  handle:close()
-  os.execute('javac -d class " + result)
+	local handle = io.popen('grep -H "public static void main" *.java | cut -d: -f1')
+	local result = handle:read("*a") -- liest die gesamte Ausgabe
+	handle:close()
+	os.execute("javac -d class " + result)
 	os.execute("java HelloWorld")
 end
 
