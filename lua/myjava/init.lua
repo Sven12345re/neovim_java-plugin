334c38846fb6f1cb392496d4fe55a08652c4636a
Sven12345re <sven.redlich96@gmail.com>
2025-09-26 01:07:00 +0200

    test2

--- lua/myjava/init.lua
+++ lua/myjava/init.lua
@@ -8,12 +8,12 @@ end
 
 function M.compileAndnRun()
 	OpenOutput()
-	local handle = io.popen('grep -H "public static void main" *.java | cut -d: -f1')
-	local result = handle:read("*a") -- liest die gesamte Ausgabe
-	local runString = string.gsub(result, ".java", "")
-	handle:close()
-	os.execute("javac -d class " .. result)
-	os.execute("java -cp class " .. runString)
+	--	local handle = io.popen('grep -H "public static void main" *.java | cut -d: -f1')
+	-- local result = handle:read("*a") -- liest die gesamte Ausgabe
+	-- local runString = string.gsub(result, ".java", "")
+	-- handle:close()
+	-- os.execute("javac -d class " .. result)
+	-- os.execute("java -cp class " .. runString)
 end
 
 function OpenOutput()
