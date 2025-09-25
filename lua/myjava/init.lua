ed89655b505de3c1d93ba1a775b81fc10197464c
Sven12345re <sven.redlich96@gmail.com>
2025-09-25 23:24:22 +0200

    add run string

--- lua/myjava/init.lua
+++ lua/myjava/init.lua
@@ -9,9 +9,10 @@ end
 function M.compileAndnRun()
 	local handle = io.popen('grep -H "public static void main" *.java | cut -d: -f1')
 	local result = handle:read("*a") -- liest die gesamte Ausgabe
+  local runString = s = string.gsub(result, ".java", "")
 	handle:close()
 	os.execute("javac -d class " .. result)
-	os.execute("java -cp class" .. result .. "| cut -d.java -f1")
+	os.execute("java -cp class" .. runString)
 end
 
 return M
