9b573da55b829159a8720e96a0320cf050b7d01f
Sven12345re <sven.redlich96@gmail.com>
2025-09-25 23:27:00 +0200

    fix run string

--- lua/myjava/init.lua
+++ lua/myjava/init.lua
@@ -9,7 +9,7 @@ end
 function M.compileAndnRun()
 	local handle = io.popen('grep -H "public static void main" *.java | cut -d: -f1')
 	local result = handle:read("*a") -- liest die gesamte Ausgabe
-  local runString = s = string.gsub(result, ".java", "")
+	local runString = string.gsub(result, ".java", "")
 	handle:close()
 	os.execute("javac -d class " .. result)
 	os.execute("java -cp class" .. runString)
