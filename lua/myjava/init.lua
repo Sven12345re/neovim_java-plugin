c925e41f35429da7fbafb611f28dfd6c483af1cd
Sven12345re <sven.redlich96@gmail.com>
2025-09-26 00:36:32 +0200

    adapt terminal

--- lua/myjava/init.lua
+++ lua/myjava/init.lua
@@ -7,8 +7,24 @@ function M.greeting()
 end
 
 function M.compileAndnRun()
-	io.write("\27[2J") -- Bildschirm löschen
-	io.write("\27[H") -- Cursor nach oben links setzen
+	local text = "Lua rocks!"
+
+	-- Terminalgröße holen
+	local handle = io.popen("stty size", "r")
+	local result = handle:read("*a")
+	handle:close()
+
+	local rows, cols = result:match("(%d+)%s+(%d+)")
+	rows, cols = tonumber(rows), tonumber(cols)
+
+	-- Berechnen wo Text hin soll
+	local row = math.floor(rows / 3) -- etwas höher als Mitte
+	local col = math.floor((cols - #text) / 2)
+
+	-- Bildschirm löschen + schreiben
+	io.write("\27[2J")
+	io.write(string.format("\27[%d;%dH%s\n", row, col, text))
+
 	local handle = io.popen('grep -H "public static void main" *.java | cut -d: -f1')
 	local result = handle:read("*a") -- liest die gesamte Ausgabe
 	local runString = string.gsub(result, ".java", "")
