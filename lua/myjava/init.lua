2d55c0a25cad57b8999d822b3a44f1a45dd84705
Sven12345re <sven.redlich96@gmail.com>
2025-09-26 01:09:31 +0200

    test3

--- lua/myjava/init.lua
+++ lua/myjava/init.lua
@@ -8,6 +8,22 @@ end
 
 function M.compileAndnRun()
 	OpenOutput()
+	local ns = vim.api.nvim_create_namespace("HelloWorldNS")
+
+	vim.api.nvim_create_user_command("HelloWorld", function()
+		local bufnr = vim.api.nvim_get_current_buf()
+		local last_line = vim.api.nvim_buf_line_count(bufnr)
+
+		-- Clear alte Ausgabe
+		vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
+
+		-- Setze Ausgabe als virtuellen Text unterhalb des Codes
+		vim.api.nvim_buf_set_extmark(bufnr, ns, last_line - 1, 0, {
+			virt_lines = { { { "Hello World 🌍", "Comment" } } },
+			virt_lines_above = false,
+		})
+	end, {})
+
 	--	local handle = io.popen('grep -H "public static void main" *.java | cut -d: -f1')
 	-- local result = handle:read("*a") -- liest die gesamte Ausgabe
 	-- local runString = string.gsub(result, ".java", "")
