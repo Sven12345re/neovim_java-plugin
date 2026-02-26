0cd3d54dfd57b47d81487cf51dd89d47ea9c0904
Sven12345re <sven.redlich96@gmail.com>
2026-02-27 00:03:25 +0100

    update

--- lua/myjava/init.lua
+++ lua/myjava/init.lua
@@ -6,46 +6,104 @@ function M.greeting()
 	print("Hello World neu")
 end
 
-function M.compileAndnRun()
-	OpenOutput()
-	local ns = vim.api.nvim_create_namespace("HelloWorldNS")
+function M.run()
+	local mains = vim.fn.systemlist("grep -rl 'public static void main'")
+	if #mains == 0 then
+		print("❌ Keine Main-Methode gefunden")
+		return
+	end
 
-	vim.api.nvim_create_user_command("HelloWorld", function()
-		local bufnr = vim.api.nvim_get_current_buf()
-		local last_line = vim.api.nvim_buf_line_count(bufnr)
+	-- nimm erstmal die erste gefundene Datei
+	local main_file = mains[1]
 
-		-- Clear alte Ausgabe
-		vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
+	-- 3. Dateipfad -> Klassennamen
+	-- Beispiel: ./src/com/example/App.java -> com.example.App
+	local classname = main_file
+	classname = classname:gsub("^%.", "") -- führendes ./ weg
+	classname = classname:gsub("%.java$", "") -- .java weg
+	classname = classname:gsub("src.", "") -- / -> .
 
-		-- Setze Ausgabe als virtuellen Text unterhalb des Codes
-		vim.api.nvim_buf_set_extmark(bufnr, ns, last_line - 1, 0, {
-			virt_lines = { { { "Hello World 🌍", "Comment" } } },
-			virt_lines_above = false,
-		})
-	end, {})
-
-	--	local handle = io.popen('grep -H "public static void main" *.java | cut -d: -f1')
-	-- local result = handle:read("*a") -- liest die gesamte Ausgabe
-	-- local runString = string.gsub(result, ".java", "")
-	-- handle:close()
-	-- os.execute("javac -d class " .. result)
-	-- os.execute("java -cp class " .. runString)
+	local term_buf = vim.fn.bufnr("term://*")
+	if term_buf ~= -1 then
+		vim.cmd("bd! " .. term_buf) -- Buffer killen (bd! = buffer delete)
+	end
+	vim.cmd("botright 10split | terminal java -cp out " .. classname)
 end
 
-function OpenOutput()
-	local ns = vim.api.nvim_create_namespace("HelloWorldNS")
-	vim.api.nvim_create_user_command("HelloWorld2", function()
-		local bufnr = vim.api.nvim_get_current_buf()
-		local last_line = vim.api.nvim_buf_line_count(bufnr)
+function M.compile()
+	local root = vim.fn.getcwd()
+	local out = root .. "/out"
+	local continue = true
 
-		-- Clear alte Ausgabe
-		vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
+	-- create out dir
+	vim.fn.mkdir(out, "p")
 
-		-- Setze Ausgabe als virtuellen Text unterhalb des Codes
-		vim.api.nvim_buf_set_extmark(bufnr, ns, last_line - 1, 0, {
-			virt_lines = { { { "Hello World 🌍", "Comment" } } },
-			virt_lines_above = false,
-		})
-	end, {})
+	-- find alle java files
+	local handle = io.popen("find " .. root .. " -name '*.java'")
+	local files = handle:read("*a")
+	handle:close()
+
+	-- temporäre Datei mit Fileliste
+	local listfile = root .. "/sources.txt"
+	local f = io.open(listfile, "w")
+	f:write(files)
+	f:close()
+
+	local stdout_data = {}
+	local stderr_data = {}
+	local term_buf = vim.fn.bufnr("term://*")
+	--vim.cmd("botright 10split | terminal javac @" .. listfile .. " -d " .. out)
+	local compile_job = vim.fn.jobstart({ "javac", "@" .. listfile, "-d", out }, {
+		stdout_buffered = true,
+		stderr_buffered = true,
+		on_stdout = collect_output(stdout_data),
+		on_stderr = collect_output(stderr_data),
+	})
+
+	-- wartet max. 30 Sekunden
+	local result = vim.fn.jobwait({ compile_job }, 30000)
+	vim.notify("wait")
+	if result[1] == 0 then
+		return stdout_data
+	elseif result[1] == -1 then
+		print("Jobwait timed out")
+	else
+		print("Compile failed with exit code: " .. result[1])
+	end
+	-- compile
+	--	vim.fn.jobstart({ "javac", "@" .. listfile, "-d", out }, {
+	--	stdout_buffered = true,
+	--on_stdout = function(_, data)
+	--vim.notify(table.concat(data, "\n"), vim.log.levels.INFO)
+	--	if data then
+	--	vim.list_extend(stdout_data, data)
+	--	vim.notify("test")
+	-- M.run()
+	--	end
+	--end,
+	--on_stderr = function(_, data)
+	--	if data then
+	--		vim.notify(table.concat(data, "\n"), vim.log.levels.ERROR)
+	--		local term_buf = vim.fn.bufnr("term://*")
+	--		if term_buf ~= -1 then
+	-- vim.cmd("bd! " .. term_buf) -- Buffer killen (bd! = buffer delete)
+	--	end
+	--	end
+	--end,
+	--})
+	--
 end
+
+function collect_output(target_table)
+	return function(_, data)
+		if data then
+			for _, line in ipairs(data) do
+				if line ~= "" then
+					table.insert(target_table, line)
+				end
+			end
+		end
+	end
+end
+
 return M
--- plugin/myjava.lua
+++ plugin/myjava.lua
@@ -4,5 +4,17 @@ vim.api.nvim_create_user_command("HelloWorld", function()
 end, {})
 
 vim.api.nvim_create_user_command("CompileAndRunJava", function()
-	require("myjava").compileAndnRun()
+	local test = require("myjava").compile()
+	for line in test do
+		print(line)
+	end
+	--require("myjava").run()
+end, {})
+
+vim.api.nvim_create_user_command("CompileJava", function()
+	require("myjava").compile()
+end, {})
+
+vim.api.nvim_create_user_command("RunJava", function()
+	require("myjava").run()
 end, {})
